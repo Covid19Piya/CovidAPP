254dfa1eeb1df69326dc8820401f86b5b6cd72e3
zeroegoo <60128150+zeroegoo@users.noreply.github.com>
2021-09-03 12:54:54 +0700

    เขียนอธิบาย

--- Patient/MenuPatient.js
+++ Patient/MenuPatient.js
@@ -1,55 +1,91 @@
 import * as React from 'react';
-import {useContext, useEffect} from 'react';
-import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
-import {AuthContext} from '../navigaiton/AuthProvider';
+import { useContext, useEffect } from 'react';
+import { Image, View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { AuthContext } from '../navigaiton/AuthProvider';
 import PushNotification from "react-native-push-notification";
 
 
-export default function homeScreenStudent({navigation}) {
-  const {user, logout} = useContext(AuthContext);
+export default function homeScreenStudent({ navigation }) {
+  const { user, logout } = useContext(AuthContext);
 
   useEffect(() => {
     createChannels();
-    
+
   })
-  const createChannels = () =>{
+  const createChannels = () => {
     PushNotification.createChannel({
       channelId: "patient",
       channelName: "patient"
     })
   }
-  
+
 
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}> Welcome Patient </Text>
-      <Text style={styles.head}>"{user.phoneNumber}"</Text>
-
-    
-      <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('PatientForm', { user: user })}>
-        <Text style={styles.loginButtonText}>
-        Need Help
+      <View style={styles.profile}>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={styles.welcome}> สวัสดี</Text>
+          <TouchableOpacity style={{
+            width: 40, height: 40, marginTop: 25,
+            marginLeft: '47%',
+          }}>
+            <Image source={require('./chat.png')} style={{
+              width: 40, height: 40
+            }} />
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.head}>
+          <Image source={require('./user.png')} style={{ width: 34, height: 34, }} />
+          "{user.phoneNumber}"
         </Text>
-      </TouchableOpacity>
+      </View>
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('PatientForm', { user: user })}>
+          <Image source={require('./search.png')} style={{ width: 80, height: 80, marginTop: 15, marginBottom: 15, }} />
+          <Text style={styles.loginButtonText}>
+            ค้นหาผู้ให้
+          </Text>
+          <Text style={styles.loginButtonText}>
+            ความช่วยเหลือ
+          </Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('PatientStatus', { user: user })}>
-        <Text style={styles.loginButtonText}>
-        Your Case
-        </Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('PatientForm', { user: user })}>
+          <Image source={require('./help.png')} style={{ width: 92, height:80, marginTop: 15, marginBottom: 15, }} />
+          <Text style={styles.loginButtonText}>
+            โพสขอ
+          </Text>
+          <Text style={styles.loginButtonText}>
+            ความช่วยเหลือ
+          </Text>
+        </TouchableOpacity>
+      </View>
 
-      <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('PatientChat', { user: user })}>
-        <Text style={styles.loginButtonText}>
-        Chat
-        </Text>
-      </TouchableOpacity>
-      
-      <TouchableOpacity style={styles.logoutButton} onPress={() => logout()}>
-        <Text style={styles.loginButtonText}>
-        Logout
-        </Text>
-      </TouchableOpacity>
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('PatientStatus', { user: user })}>
+          <Image source={require('./status.png')} style={{ width: 70, height: 90, marginTop: 15, marginBottom: 8, }} />
+          <Text style={styles.loginButtonText}>
+          คำขอ
+          </Text>
+          <Text style={styles.loginButtonText}>
+          ให้การช่วยเหลือ
+          </Text>
+        </TouchableOpacity>
 
+        <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('PatientChat', { user: user })}>
+          <Image source={require('./news.png')} style={{ width: 99, height: 90, marginTop: 15, marginBottom: 15, }} />
+          <Text style={styles.loginButtonText}>
+            ข่าวสาร
+          </Text>
+        </TouchableOpacity>
+        </View>
+      <View style={{ alignItems: "center", }}>  
+        <TouchableOpacity style={styles.logoutButton} onPress={() => logout()}>
+          <Text style={styles.logoutButtonText}>
+            ออกจากระบบ
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -59,57 +95,76 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     textAlign: 'center',
   },
-  welcome:{
-    fontWeight: 'bold',
-    fontSize:25,
-    color: '#00B3B2',
-  },
-  head:{
-    fontWeight: 'bold',
-    fontSize:20,
-    color: '#00B3B2',
-    marginBottom: 20,
-  },
+
   input: {
     marginVertical: 10,
     marginBottom: 15,
   },
-  loginButton: {
-    marginVertical: 10,
-    backgroundColor: '#DFF17C',
-    width: 320,
-    height: 60,
-    borderRadius: 10,
-    shadowColor: "#000000",
-    shadowOpacity: 5,
-    shadowRadius: 5,
-    elevation: 5
-  },
-  logoutButton: {
-    marginVertical: 10,
-    backgroundColor: '#b53531',
-    width: 320,
-    height: 60,
-    borderRadius: 10,
-    shadowColor: "#000000",
-    shadowOpacity: 5,
-    shadowRadius: 5,
-    elevation: 5
-  },
-  loginButtonText: {
-    textAlign: 'center',
-    color: '#F0FFFF',
-    fontWeight: 'bold',
-    fontSize:20,
-    padding: 15
+
+  profile: {
+    backgroundColor: '#5BC0DE',
   },
 
+  welcome: {
+    marginTop: 10,
+    marginLeft: 15,
+    fontWeight: 'bold',
+    fontSize: 45,
+    color: '#FFFFFF',
+  },
+
+  head: {
+    marginLeft: 15,
+    fontWeight: 'bold',
+    fontSize: 25,
+    color: '#FFFFFF',
+    marginBottom: 20,
+  },
+
+  loginButtonText: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+
+  loginButton: {
+    alignItems: "center",
+    marginLeft: 10,
+    marginRight: 4,
+    marginVertical: 10,
+    backgroundColor: '#5BC0DE',
+    width: 180,
+    height: 181,
+    borderRadius: 10,
+    shadowColor: "#000000",
+    shadowOpacity: 5,
+    shadowRadius: 5,
+    elevation: 5
+  },
+
+  logoutButtonText: {
+    marginTop: 15,
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+
+  logoutButton: {
+    marginTop: 20,
+    backgroundColor: '#FF1E1E',
+    width: 150,
+    height: 60,
+    borderRadius: 10,
+    shadowColor: "#000000",
+    shadowOpacity: 5,
+    shadowRadius: 5,
+    elevation: 5
+  },
 
   container: {
     flex: 1,
-    backgroundColor: '#E2FCFA',
-    alignItems: 'center',
-    paddingHorizontal: 20,
-    paddingTop: 100,
+    backgroundColor: '#B5DBFD',
   },
 });
--- Volunteer/MenuVolunteer.js
+++ Volunteer/MenuVolunteer.js
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { useContext, Component, useEffect } from 'react';
-import { View, StyleSheet, Text, Alert, TouchableOpacity } from 'react-native';
+import { Image, View, StyleSheet, Text, Alert, TouchableOpacity } from 'react-native';
 import { AuthContext } from '../navigaiton/AuthProvider';
 import PushNotification from "react-native-push-notification";
+import { Directions } from 'react-native-gesture-handler';
+import { Center } from 'native-base';
 
 
 export default function homeScreen({ navigation }) {
@@ -11,8 +13,8 @@ export default function homeScreen({ navigation }) {
   useEffect(() => {
     createChannels();
   })
-  
-  const createChannels = () =>{
+
+  const createChannels = () => {
     PushNotification.createChannel({
       channelId: "test",
       channelName: "test"
@@ -22,7 +24,7 @@ export default function homeScreen({ navigation }) {
   const handleNotification = (item) => {
     PushNotification.localNotification({
       channelId: "test",
-      title : "You "+item,
+      title: "You " + item,
       message: item
     })
     console.log("eiei")
@@ -30,41 +32,74 @@ export default function homeScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}> Welcome Volunteer </Text>
-      <Text style={styles.head}>"{user.email}"</Text>
-
-      <TouchableOpacity style={styles.loginButton} onPress={() => {navigation.navigate('Find Patient', { user: user });
-    handleNotification("eiei")}}>
-        <Text style={styles.loginButtonText}>
-          Looking for patient
+      <View style={styles.profile}>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={styles.welcome}> สวัสดี</Text>
+          <TouchableOpacity style={{
+            width: 40, height: 40, marginTop: 25,
+            marginLeft: '47%',
+          }}>
+            <Image source={require('./chat.png')} style={{
+              width: 40, height: 40
+            }} />
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.head}>
+          <Image source={require('./user.png')} style={{ width: 34, height: 34, }} />
+          "{user.email}"
         </Text>
-      </TouchableOpacity>
+      </View>
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity style={styles.loginButton} onPress={() => {
+          navigation.navigate('Find Patient', { user: user });
+          handleNotification("eiei")
+        }}>
+          <Image source={require('./search.png')} style={{ width: 80, height: 80, marginTop: 15, marginBottom: 15, }} />
+          <Text style={styles.loginButtonText}>
+            ค้นหาคนที่ต้องการ
+          </Text>
+          <Text style={styles.loginButtonText}>
+            ความช่วยเหลือ
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('Your Case', { user: user })}>
+          <Image source={require('./aid.png')} style={{ width: 90, height: 90, marginTop: 15, marginBottom: 15, }} />
+          <Text style={styles.loginButtonText}>
+            สถานะ
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('Post Donate')}>
+          <Image source={require('./donate.png')} style={{ width: 100, height: 80, marginTop: 15, marginBottom: 15, }} />
+          <Text style={styles.loginButtonText}>
+            โพสให้
+          </Text>
+          <Text style={styles.loginButtonText}>
+            ความช่วยเหลือ
+          </Text>
+        </TouchableOpacity>
 
 
-      <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('Your Case', {user: user })}>
-        <Text style={styles.loginButtonText}>
-          Patient Case
-        </Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('News Donate')}>
+          <Image source={require('./news.png')} style={{ width: 99, height: 90, marginTop: 15, marginBottom: 15, }} />
+          <Text style={styles.loginButtonText}>
+            ข่าวสาร
+          </Text>
+        </TouchableOpacity>
+      </View>
 
-      <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('Post Donate')}>
-        <Text style={styles.loginButtonText}>
-          Post for donate
-        </Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('News Donate')}>
-        <Text style={styles.loginButtonText}>
-          News
-        </Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.logoutButton} onPress={() => logout()}>
-        <Text style={styles.loginButtonText}>
-          Logout
-        </Text>
-      </TouchableOpacity>
+      <View style={{ alignItems: "center", }}>
+        <TouchableOpacity style={styles.logoutButton} onPress={() => logout()}>
+          <Text style={styles.logoutButtonText}>
+            ออกจากระบบ
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
+
   );
 }
 
@@ -73,46 +108,66 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     textAlign: 'center',
   },
+
   input: {
     marginVertical: 10,
     marginBottom: 15,
   },
+
+  profile: {
+    backgroundColor: '#5BC0DE',
+  },
+
   welcome: {
+    marginTop: 10,
+    marginLeft: 15,
+    fontWeight: 'bold',
+    fontSize: 45,
+    color: '#FFFFFF',
+  },
+
+  head: {
+    marginLeft: 15,
     fontWeight: 'bold',
     fontSize: 25,
-    color: '#00B3B2',
-
-  },
-  head: {
-    fontWeight: 'bold',
-    fontSize: 20,
-    color: '#00B3B2',
+    color: '#FFFFFF',
     marginBottom: 20,
   },
 
   loginButtonText: {
     textAlign: 'center',
-    color: '#F0FFFF',
+    color: '#FFFFFF',
     fontWeight: 'bold',
     fontSize: 20,
-    padding: 15
   },
 
   loginButton: {
+    alignItems: "center",
+    marginLeft: 10,
+    marginRight: 4,
     marginVertical: 10,
-    backgroundColor: '#DFF17C',
-    width: 320,
-    height: 60,
+    backgroundColor: '#5BC0DE',
+    width: 180,
+    height: 181,
     borderRadius: 10,
     shadowColor: "#000000",
     shadowOpacity: 5,
     shadowRadius: 5,
     elevation: 5
   },
+
+  logoutButtonText: {
+    marginTop: 15,
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+
   logoutButton: {
-    marginVertical: 10,
-    backgroundColor: '#b53531',
-    width: 320,
+    marginTop: 20,
+    backgroundColor: '#FF1E1E',
+    width: 150,
     height: 60,
     borderRadius: 10,
     shadowColor: "#000000",
@@ -123,11 +178,6 @@ const styles = StyleSheet.create({
 
   container: {
     flex: 1,
-    backgroundColor: '#E2FCFA',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-
-
+    backgroundColor: '#B5DBFD',
   },
 });
